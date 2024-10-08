ee276759e3e5fe37791e493e655917ebf773fe48
juccca <j_campos2010@outlook.com>
2024-10-08 11:51:11 -0300

    Configuração do Banco de Dados e da Tabela Veículo

---
+++ mf-dev-back-end-1/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace mf_dev_back_end_1.Models
+{
+    //cria clsse AppDbContext para gerenciar o contexto
+    public class AppDbContext : DbContext //faz herança DbContext, classe ppDbContext passa a herdar todas as características e propriedades do DbContext
+    {
+        //no construtor AppDbContext, o DbContextOptions recebe, da classe AppDbContext, options e passa como base para a herança DbContext
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        //associa tabela de banco de dados a entidade "Veiculos", que é do tipo DbSet
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
--- mf-dev-back-end-1/Program.cs
+++ mf-dev-back-end-1/Program.cs
@@ -1,9 +1,16 @@
+using mf_dev_back_end_1.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+//cria modelo de dados
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
